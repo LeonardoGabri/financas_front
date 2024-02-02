8de3215c9529f61055a07b26522d5a9571472d2e
Leonardo Gabriel <leonardogabriel8@hotmail.com>
2024-02-02 18:25:35 -0300

    Ajustando var ambi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   url: {
     //service: 'https://financasapi-production.up.railway.app'
-    service: 'http://localhost:8083'
+    service: '${URL_SERVICE}'
   }
 }
